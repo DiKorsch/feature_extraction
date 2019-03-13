bf9351391c734ebce8ae0747eea67fd6e0c4d140
Dimitri Korsch <dimitri.korsch@uni-jena.de>
2019-03-13 09:28:21 +0100

    moved preprocessing functions to utils module

--- run.py
+++ run.py
@@ -9,6 +9,7 @@ import numpy as np
 
 from core.dataset import Dataset#, TFDataset
 from utils.arguments import extract_args, ModelType
+from utils.preprocessing import prepare, prepare_tf
 from chainer.iterators import SerialIterator, MultiprocessIterator
 from chainercv.transforms import center_crop, resize
 from nabirds import CUB_Annotations
@@ -17,64 +18,13 @@ import matplotlib.pyplot as plt
 from functools import partial
 from tqdm import tqdm
 
-def _prepare(im, size, crop_fraction=0.875):
-	crop_size = None
-	h, w, c = im.shape
-
-	if isinstance(size, int):
-		size = (size, size)
-
-	_im = im.transpose(2, 0, 1)
-
-	if crop_fraction:
-		crop_size = (np.array([h, w]) * crop_fraction).astype(np.int32)
-		_im = center_crop(_im, crop_size)
-
-	_im = resize(_im, size, interpolation=2)
-	if _im.dtype == np.uint8:
-		# rescale [0 .. 255] -> [0 .. 1]
-		_im = (_im / 255).astype(np.float32)
-
-	# rescale [0 .. 1] -> [-1 .. 1]
-	return _im * 2 - 1
-
-def _prepare_tf(size, crop_fraction=0.875, from_path=False):
-
-	config_sess = tf.ConfigProto(allow_soft_placement=True)
-	config_sess.gpu_options.allow_growth = True
-	sess = tf.Session(config=config_sess)
-
-	if from_path:
-		im_input = tf.placeholder(tf.string)
-		image = tf.image.decode_jpeg(tf.read_file(im_input), channels=3)
-		image = tf.image.convert_image_dtype(image, tf.float32)
-	else:
-		image = im_input = tf.placeholder(tf.float32, shape=(None, None, 3))
-
-
-	image = tf.image.central_crop(image, central_fraction=crop_fraction)
-	image = tf.expand_dims(image, 0)
-	image = tf.image.resize_bilinear(image, [size, size], align_corners=False)
-	image = tf.squeeze(image, [0])
-	image = tf.subtract(image, 0.5)
-	output = tf.multiply(image, 2)
-
-	def inner(im):
-		if not from_path and im.dtype == np.uint8:
-			im = im / 255
-
-		res = sess.run(output, feed_dict={im_input: im})
-		return res.transpose(2, 0, 1)
-
-	return inner
-
 def main(args):
 	if args.debug:
 		chainer.set_debug(args.debug)
 		logging.warning("DEBUG MODE ON!")
 	GPU = args.gpu[0]
 
-	model, prepare = ModelType.new(args.model_type)
+	model, model_prepare = ModelType.new(args.model_type)
 	model.load_for_inference(weights=args.weights, n_classes=args.n_classes)
 
 
@@ -85,14 +35,14 @@ def main(args):
 	annot = CUB_Annotations(args.data)
 
 	# model's prepare
-	# prepare_func = prepare
+	# prepare_func = model_prepare
 	# custom prepare
-	prepare_func = partial(_prepare, size=model.meta.input_size)
+	prepare_func = partial(prepare, size=model.meta.input_size)
 	# TensorFlow's prepare without image reading
-	# prepare_func = _prepare_tf(model.meta.input_size)
+	# prepare_func = prepare_tf(model.meta.input_size)
 	data = Dataset(args, annot, prepare=prepare_func)
 	# TensorFlow's prepare with image reading
-	# prepare_func = _prepare_tf(model.meta.input_size, from_path=True)
+	# prepare_func = prepare_tf(model.meta.input_size, from_path=True)
 	# data = TFDataset(args, annot, prepare=prepare_func)
 
 	logging.info("Loaded dataset with {} samples from \"{}\"".format(
